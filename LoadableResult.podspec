d0a1828144aba4c6859129013e80b6aa2bffd40b
ancona-esselmann <axel@anconaesselmann.com>
2020-02-28 15:57:02 -0800

    Adds additional mapping and documentation

--- LoadableResult.podspec
+++ LoadableResult.podspec
@@ -1,44 +1,20 @@
-#
-# Be sure to run `pod lib lint LoadableResult.podspec' to ensure this is a
-# valid spec before submitting.
-#
-# Any lines starting with a # are optional, but their use is encouraged
-# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
-#
-
 Pod::Spec.new do |s|
   s.name             = 'LoadableResult'
-  s.version          = '0.2.0'
+  s.version          = '0.2.1'
   s.summary          = 'LoadableResult is a Result type with a loading state'
   s.swift_version    = '5.0'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
 LoadableResult allows services to expose a loading state.
                        DESC
 
   s.homepage         = 'https://github.com/anconaesselmann/LoadableResult'
-  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'ancona-esselmann' => 'axel@anconaesselmann.com' }
   s.source           = { :git => 'https://github.com/anconaesselmann/LoadableResult.git', :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '10.0'
   s.watchos.deployment_target = '3.0'
 
   s.source_files = 'LoadableResult/Classes/**/*'
-
-  # s.resource_bundles = {
-  #   'LoadableResult' => ['LoadableResult/Assets/*.png']
-  # }
-
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
 end
--- LoadableResult/Classes/LoadableResult.swift
+++ LoadableResult/Classes/LoadableResult.swift
@@ -18,15 +18,63 @@ public enum LoadableResult<T> {
         }
     }
 
-    // TODO: Very basic implementation of map. Does not handle exceptions.
+    /// Every state can be remapped.
+    /// map should be used when a states get combined or changed altogether.
+    /// mapLoaded exists if you want to only map the loaded case, which is the more common case.
+    ///
+    /// Example:
+    /// A server response returns a valid object that should actually be treated as an error case.
     public func map<E>(_ transform: @escaping (LoadableResult<T>) -> E) -> E {
         return transform(self)
     }
 
+    /// The loaded state gets transformed to a new type.
+    /// In case acces to each individual state is necessary use map.
+    ///
+    /// Example:
+    /// - A server response returns a full object. We are only interested in in one property.
+    /// - A server response returns a list of objects. We are only interested in the first element in the list
+    public func mapLoaded<E>(_ transform: @escaping (T) -> E) -> LoadableResult<E> {
+        switch self {
+        case .inactive:
+            return .inactive
+        case .loading:
+            return .loading
+        case .loaded(let loaded):
+            return .loaded(transform(loaded))
+        case .error(let error):
+            return .error(error)
+        }
+    }
+
+    /// A network request might return an error that needs to be processed before we display it for the user
+    public func mapError<ErrorType>(_ transform: @escaping (Error) -> ErrorType) -> Self where ErrorType: Error {
+        switch self {
+        case .inactive, .loading, .loaded:
+            return self
+        case .error(let error):
+            return .error(transform(error))
+        }
+    }
+
+    /// A server request returns an error case from which we might be able to recover before with local data.
+    public func mapError(_ transform: @escaping (Error) -> T?) -> Self {
+        switch self {
+        case .inactive, .loading, .loaded:
+            return self
+        case .error(let error):
+            if let recovered = transform(error) {
+                return .loaded(recovered)
+            } else {
+                return self
+            }
+        }
+    }
+
 }
 
-/// Error types are not compared to determine equality, only the fact that an error occured.
 extension LoadableResult: Equatable where T: Equatable {
+    /// Note: Error types are not compared to determine equality, only the fact that an error occured.
     public static func == (lhs: LoadableResult<T>, rhs: LoadableResult<T>) -> Bool {
         switch (lhs, rhs) {
         case (.loading, .loading): return true
